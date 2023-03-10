e89fbba271234922d28cb8166f44c205bd23973e
Pritam <patrapritam0099@gmail.com>
2023-03-10 21:46:02 +0530

    commit

--- 1_03_10_2023.py
+++ 1_03_10_2023.py
@@ -6,8 +6,8 @@ print(5)
 
 # how to use if condition in python language.
 
-a = 5
-b = 8
+a = 5 # where a is a variable and value of a is 5 .
+b = 8 # where b is a variable and value of b is 8 .
 
 if a > b :
     print("where a is greter than b ")
@@ -23,3 +23,29 @@ i realised if you exits in my life i do better in future compare to my present l
 i just thought every deep night when i deep sleep aqnd that place only present you and me .
 realy i love you .
 """
+
+# If you want to specify the data type of a variable, this can be done with casting.
+
+x = str(8)    # x will be '8'
+y = int(9)    # y will be 9
+z = float(10)  # z will be 10.0
+w = 4.0
+print("where the type of x is string :", x,type(x))
+print("where the type of y is intger :", y, type(y))
+print("where the type of z is float :", z, type(z))
+print(type(w))
+
+str1 = "sougata"
+str2 = "pritam"
+str3 = "koushik"
+
+print(str1+" "+str2+" "+str3)
+
+
+num1 = 12
+num2 = 13
+num3 = 14
+num4 = 15
+
+
+print(num1+num2+num3+num4)
