84e2cd73218d38cd579aacf7d6f8e8fc3e0940ab
Pritam <patrapritam0099@gmail.com>
2023-03-10 20:24:30 +0530

    commit

---
+++ 1_03_10_2023.py
@@ -0,0 +1,25 @@
+
+# prin any value or any things by python language.
+
+print("Hello pritam patra....")
+print(5)
+
+# how to use if condition in python language.
+
+a = 5
+b = 8
+
+if a > b :
+    print("where a is greter than b ")
+else :
+    print("where b is greter than a ")
+
+# how to use multi line comments in python language .
+
+"""
+i love you.
+i like you .
+i realised if you exits in my life i do better in future compare to my present life.
+i just thought every deep night when i deep sleep aqnd that place only present you and me .
+realy i love you .
+"""
