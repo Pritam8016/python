0ceee9d5d80930f5d722bfb332f5b2227913b910
pritam <patrapritam0099@gmail.com>
2023-03-17 00:47:55 +0530

    pritam

---
+++ 3_03_16_2023.py
@@ -0,0 +1,28 @@
+# data type :
+# 1. str type
+a = "twentyfive"
+print(type(a))
+ 
+# numeric type
+
+b = 25
+print(type(b))
+      
+c = 25.5
+print(type(c))
+
+d = 25j 
+print(type(d))  # display the data type of d :
+
+e = ["apple" , "banana" , "mango"]
+print(type(e))
+
+f = ("apple" , "banana" , "mango")
+print(type(f))
+
+i = range(6)
+print(i)
+print(type(i))
+
+j = {"name" : "pritam" , "age" : "27"}
+print(type(j))
